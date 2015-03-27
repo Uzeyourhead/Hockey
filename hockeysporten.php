24fb0332f1c9cac56b6f0804d4b2c0ac536256ae
Uzeyourhead <uzeyourhead@hotmail.com>
2015-03-27 10:48:49 +0100

    mapp

--- hockeysporten.php
+++ hockeysporten.php
@@ -88,7 +88,7 @@
   </div><!-- /.container-fluid -->
 </nav>
 
-<a class="undermeny" href="#">Nyheter</a>
+<a class="undermeny" href="artikel.php">Nyheter</a>
 <a class="undermeny" href="#">Nyheter</a>
 <a class="undermeny" href="#">Nyheter</a>
 <a class="undermeny" href="#">Nyheter</a>
@@ -115,67 +115,27 @@
   		<div class="news"> 
 
 <div class="slidecontainer">
-
+<div class="flexslider">
+  <ul class="slides">
+    <li>
       <img src="images/hall.jpg" />
-
+    </li>
+    <li>
+      <img src="images/hall.jpg" />
+    </li>
+    <li>
+      <img src="images/hall.jpg" />
+    </li>
+    <li>
+      <img src="images/hall.jpg" />
+    </li>
+  </ul>
+</div> 
 </div>
 
         <div class="newscontainer">
 
-          <h3>Alfredsson avtackades i natt</h3>
-          <p>Skrivet av Martin Ericsson inom Hockey, NHL, Ottawa Senators, Daniel Alfredsson den 5 Dec 2014 kl 08:18</p>
-          <hr></hr>
-
-          <div class="subarticle">
-          <strong><p>Daniel Alfredsson tackades av i Ottawa i natt, och det blev känslosamt för många. Han spenderade 17 säsonger i klubben och jublet visste inga gränser när han återigen beträdde isen i arenan.
-          – Tack så enormt mycket, sa Alfredsson under sitt tal.</p></strong>
-          </div>
-          <div class="taggzon">
-            <ul class="tagglist">
-              <li>Bobrovsky satte klubbrekord</li>
-              <li>Bobrovsky satte klubbrekord</li>
-              <li>Bobrovsky satte klubbrekord</li>
-              <li>Bobrovsky satte klubbrekord</li>
-            </ul>
-          </div>
-
-          <p style="text-align:justify; width:500px;">Daniel Alfredsson tackades av i Ottawa i natt, och det blev känslosamt för många. Han spenderade 17 säsonger i klubben och jublet visste inga gränser när han återigen beträdde</p>
-          <p class="articlep">Daniel Alfredsson tackades av i Ottawa i natt, och det blev känslosamt för många. Han spenderade 17 säsonger i klubben och jublet visste inga gränser när han återigen beträdde isen i arenan.
-          – Tack så enormt mycket, sa Alfredsson under sitt tal.Daniel Alfredsson tackades av i Ottawa i natt, och det blev känslosamt för många. Han spenderade 17 säsonger i klubben och jublet visste inga gränser när han återigen beträdde isen i arenan.
-          – Tack så enormt mycket, sa Alfredsson under sitt tal.I går kom avskedet från NHL-världen på en presskonferens och när laget sedan tog emot New York Islanders på hemmaplan tackades han av utav Ottawa-publiken. Och det blev minst sagt en kväll att minnas för alla inblandade.
-
-Publiken skanderade "Alfie, Alfie, Alfie" om och om igen medan 41-åringen åkte runt på isen med klubban höjd mot taket. Efteråt höll han ett kort tal för publiken och sina gamla lagkamrater och ledare.
-
-– Tack så enormt mycket. Det är fantastiskt att vara tillbaka i Ottawa. Tack för alla minnen. De kommer att förbinda oss för evigt. Men åt oss inte säga farväl, låt oss säga på återseende, sa han bland annat.</p>
-   
-<p>Dela med dig av "Alfredsson avtackades i natt" till dina vänner:</p>
-
-<h4>Like knappar</h4>
-
-<div class="skribent">
-
-  <img class="profilbild" src="images/martin.jpg">
-
-  <div class="skribentinfo">
-    <p class="namn">Martin Eriksson</p>
-    <p class="titel">Chefsredaktör</p>
-    <hr class="line">
-    <p class="mer">Läs mer av Martin Ericsson     Maila Martin Ericsson     mareri85</p>
-  </div>
-
-</div>
-
-<div class="comments">
-</div>
-</div>
-
-<br>
-
-
-<div class="news">
-
-<div class="newscontainer">
-    <h2>NHL-nyheter</h2>
+          <img class="newsbanner" src="images/banner.gif">
 
         <div class="rows">
           <img src="images/1.png">
@@ -233,10 +193,17 @@ Publiken skanderade "Alfie, Alfie, Alfie" om och om igen medan 41-åringen åkte
           <p>test</p>
         </div>
 
+        <div class="rows">
+          <img src="images/1.png">
+          <h3>test</h3>
+          <p>test</p>
+        </div>
 
-
-
-      </div>
+        <div class="rows">
+          <img src="images/1.png">
+          <h3>test</h3>
+          <p>test</p>
+        </div>
 
 
 
