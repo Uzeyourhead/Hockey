d5d2ac227d85e04093934dd306b4e20bca262ff8
Hazbir <christophera.rickman@gmail.com>
2015-03-27 11:58:06 +0100

    Revert "Revert "mapp""
    This reverts commit 85e7425f4aee5af8a9ecf04445349edae7de18a8.

---
+++ leagues.php
@@ -0,0 +1,16 @@
+<?php
+CREATE TABLE Leagues
+(
+ID int  NOT NULL AUTO_INCREMENT,
+name varchar(255),
+URL varchar(255),
+scoreW varchar(255),
+scoreOW varchar(255),
+scoreOL varchar(255),
+CDP varchar(255),
+CSPL varchar(255),
+image varchar(255),
+PRIMARY KEY (ID)
+);
+
+?>
