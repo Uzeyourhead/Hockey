e498fd6ee6af42543a67b39e61520d27a150f174
Hazbir <christophera.rickman@gmail.com>
2015-03-30 12:39:32 +0200

    MYSQL
    Ändrade filer som skapar databaserna.

--- createtable.sql
+++ createtable.sql
@@ -1,1 +1,23 @@
-CREATE DATABASE IF NOT EXISTS `hockey`;
+
+<?php
+session_start();
+include_once("config.php");
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+// sql to create table
+$sql = "CREATE DATABASE IF NOT EXISTS `hockey`";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+?>
+
