37d6f4ba11c5eb63517d65229e8461b537066a73
Hazbir <christophera.rickman@gmail.com>
2015-04-01 10:37:18 +0200

    Ändringar

--- config.php
+++ config.php
@@ -1,15 +1,7 @@
 <?php
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/create
 $db_username = 'root';
 $db_password = '';
 $db_name = 'hockey';
 $db_host = 'localhost';
 $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/create
 ?>
